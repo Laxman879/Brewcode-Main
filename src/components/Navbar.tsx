776bfa59f7bff7bfd7e38283dba30cca0e584d50
Laxman879 <laxman.a@brewcode.co>
2024-04-18 18:37:06 +0530

    logo added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { Navbar, Nav, Container, Dropdown } from "react-bootstrap";
 import Link from "next/link";
 import { BiChevronUp, BiChevronDown } from "react-icons/bi";
+import Image from "next/image";
 
 function Header() {
   const router = useRouter();
@@ -19,9 +20,19 @@ function Header() {
   };
 
   return (
-    <Navbar expand="lg" bg="dark" variant="dark">
+    <Navbar expand="lg" bg="dark" variant="dark" className="navbar">
       <Container>
-        <Navbar.Brand>Mega Menu</Navbar.Brand>
+        <Navbar.Brand>
+          <Link href="/">
+            <Image
+              src="/images/brewcode-logo.png"
+              alt="Brewcode logo"
+              width={40}
+              height={30}
+              className="logo"
+            />
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarSupportedContent" />
         <Navbar.Collapse id="navbarSupportedContent">
           <Nav className="mx-auto mb-2 mb-lg-0">
@@ -50,121 +61,103 @@ function Header() {
                   <div className="container-fluid">
                     <div className="row">
                       <div className="col-lg-3 col-sm-6 py-4 px-xl-5 px-4">
-                        <h5 className="mb-3">Service</h5>
+                        <h5 className="mb-3 dropdown-heading">Capabilities</h5>
                         <div className="">
                           <p>
                             <a className="list-group-item" href="#">
-                              Accomodations
+                              Colud
                             </a>
                           </p>
                           <p>
                             <a className="list-group-item" href="#">
-                              Terms & Conditions
+                              Cyber Security
                             </a>
                           </p>
                           <p>
                             <a className="list-group-item" href="#">
-                              Privacy
+                              E-Commerce
                             </a>
                           </p>
                           <p>
                             <a className="list-group-item" href="#">
-                              Lorum
+                              Enterprise Application Development
                             </a>
                           </p>
                           <p>
                             <a className="list-group-item" href="#">
-                              Lorum
+                              Resourse Planning
+                            </a>
+                          </p>
+                          <p>
+                            <a className="list-group-item" href="#">
+                              Infrastrucure
+                            </a>
+                          </p>
+                        </div>
+                      </div>
+                      <div className="col-lg-3 col-sm-6 py-4 px-xl-5 px-4 ">
+                        <p className="mt-5 padding-top">
+                          <a className="list-group-item" href="#">
+                            Robotic Proccess Automation
+                          </a>
+                        </p>
+                        <p>
+                          <a className="list-group-item" href="#">
+                            Full Stack Developement
+                          </a>
+                        </p>
+                        <p>
+                          <a className="list-group-item" href="#">
+                            Digital Assurance
+                          </a>
+                        </p>
+                        <p>
+                          <a className="list-group-item" href="#">
+                            Web Development
+                          </a>
+                        </p>
+                        <p>
+                          <a className="list-group-item" href="#">
+                            UX/UI designing
+                          </a>
+                        </p>
+                      </div>
+
+                      <div className="col-lg-3 col-sm-6 py-4 px-xl-5 px-4">
+                        <h5 className="mb-3 dropdown-heading">Industries</h5>
+                        <div className="">
+                          <p>
+                            <a className="list-group-item" href="#">
+                              Automotive
+                            </a>
+                          </p>
+                          <p>
+                            <a className="list-group-item" href="#">
+                              Banking
+                            </a>
+                          </p>
+                          <p>
+                            <a className="list-group-item" href="#">
+                              Capital Markets
+                            </a>
+                          </p>
+                          <p>
+                            <a className="list-group-item" href="#">
+                              Communication & Modie
                             </a>
                           </p>
                         </div>
                       </div>
                       <div className="col-lg-3 col-sm-6 py-4 px-xl-5 px-4">
-                        <h5 className="mb-3">Service</h5>
-                        <div className="">
+                        <div className="mt-5 padding-top">
                           <p>
                             <a className="list-group-item" href="#">
-                              Accomodations
+                              Health
                             </a>
                           </p>
                           <p>
                             <a className="list-group-item" href="#">
-                              Terms & Conditions
-                            </a>
-                          </p>
-                          <p>
-                            <a className="list-group-item" href="#">
-                              Privacy
-                            </a>
-                          </p>
-                          <p>
-                            <a className="list-group-item" href="#">
-                              Lorum
-                            </a>
-                          </p>
-                          <p>
-                            <a className="list-group-item" href="#">
-                              Lorum
-                            </a>
-                          </p>
-                        </div>
-                      </div>
-                      <div className="col-lg-3 col-sm-6 py-4 px-xl-5 px-4">
-                        <h5 className="mb-3">Service</h5>
-                        <div className="">
-                          <p>
-                            <a className="list-group-item" href="#">
-                              Accomodations
-                            </a>
-                          </p>
-                          <p>
-                            <a className="list-group-item" href="#">
-                              Terms & Conditions
-                            </a>
-                          </p>
-                          <p>
-                            <a className="list-group-item" href="#">
-                              Privacy
-                            </a>
-                          </p>
-                          <p>
-                            <a className="list-group-item" href="#">
-                              Lorum
-                            </a>
-                          </p>
-                          <p>
-                            <a className="list-group-item" href="#">
-                              Lorum
-                            </a>
-                          </p>
-                        </div>
-                      </div>
-                      <div className="col-lg-3 col-sm-6 py-4 px-xl-5 px-4">
-                        <h5 className="mb-3">Service</h5>
-                        <div className="">
-                          <p>
-                            <a className="list-group-item" href="#">
-                              Accomodations
-                            </a>
-                          </p>
-                          <p>
-                            <a className="list-group-item" href="#">
-                              Terms & Conditions
-                            </a>
-                          </p>
-                          <p>
-                            <a className="list-group-item" href="#">
-                              Privacy
-                            </a>
-                          </p>
-                          <p>
-                            <a className="list-group-item" href="#">
-                              Lorum
-                            </a>
-                          </p>
-                          <p>
-                            <a className="list-group-item" href="#">
-                              Lorum
+                              High Tech
                             </a>
                           </p>
                         </div>
